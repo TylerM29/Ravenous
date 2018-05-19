a3995b9fb175d2100acf23c4eaad9b401be8a9ee
TylerM29 <37600011+TylerM29@users.noreply.github.com>
2018-05-18 19:51:49 -0600

    Update BusinessList.js

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
 import "./BusinessList.css";
+
 import Business from "../business/Business";
+
 class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
       {
         this.props.businesses.map(business => {
-          return <Business business={business} />
+          return <Business business={business} key={business.id}/>
         })
       }
 </div>
