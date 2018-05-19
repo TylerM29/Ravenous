2d66d8f9f5fb269adcaaafb5cb498dbd8e05286f
TylerM29 <37600011+TylerM29@users.noreply.github.com>
2018-05-18 19:51:16 -0600

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import "./BusinessList.css";
+import './App.css';
 
-import Business from "../business/Business";
+import BusinessList from '../BusinessList/BusinessList.js';
+import SearchBar from '../SearchBar/SearchBar.js';
+import Yelp from '../../util/Yelp';
 
-class BusinessList extends React.Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  this.state = {
+    businesses: []
+  };
+  this.searchYelp = this.searchYelp.bind(this);
+}
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
+  }
   render() {
     return (
-      <div className="BusinessList">
-      {
-        this.props.businesses.map(business => {
-          return <Business business={business} key={business.id}/>
-        })
-      }
-</div>
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
     );
   }
 }
-export default BusinessList;
+
+export default App;
